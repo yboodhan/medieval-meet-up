a26293d4ffb623957c1423be058e355a1792cd3d
yboodhan <yboodhan@gmail.com>
2020-02-13 12:22:54 -0800

    Formatted about and responsiveness on profile page, created button component

---
+++ src/Components/MatchMsgButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MatchMsgButton = props => {
+    return (
+        <div>MatchMsgButton</div>
+    )
+}
+
+export default MatchMsgButton
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Container, Col, Row } from 'reactstrap';
 
 import ProfilePicture from './ProfilePicture';
+import MatchMsgButton from './MatchMsgButton';
 
 const Profile = props => {
     return (
@@ -12,20 +13,18 @@ const Profile = props => {
                 </Col>
             </Row>
             <Row>
-                <Col>
-                    Name
+                <Col sm="12" md={{ size: 6, offset: 3 }}>
+                    <h1 className="user-name">Kitten</h1>
                 </Col>
             </Row>
             <Row>
-                <Col>
-                    About me
+                <Col sm={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }}>
+                    <p className="about-me">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur adipisci doloribus at, sunt, unde est, incidunt cupiditate beatae similique odit natus distinctio hic possimus ex facilis! Esse neque vitae libero!</p>
                 </Col>
             </Row>
             <Row>
-                <Col>
-                    <Button>
-                        Button
-                    </Button>
+                <Col sm="12" md={{ size: 6, offset: 3 }}>
+                    <MatchMsgButton />g
                 </Col>
             </Row>
         </Container>
