bff4898db8dd1f9c60c2685e783f4ae6b4a79972
yboodhan <yboodhan@gmail.com>
2020-02-13 11:57:09 -0800

    Added profile picture component

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import { Container, Col, Row } from 'reactstrap';
+import { Button, Container, Col, Row } from 'reactstrap';
 
 const Profile = props => {
     return (
         <Container>
             <Row>
-                <Col>
-                
+                <Col sm="12" md={{ size: 6, offset: 3 }}>
+                    <img className="img-fluid" src="https://placekitten.com/300/300" />
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    
+                    Name
                 </Col>
             </Row>
             <Row>
                 <Col>
-                
+                    About me
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button>
+                        Button
+                    </Button>
                 </Col>
             </Row>
         </Container>
---
+++ src/Components/ProfilePicture.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfilePicture = props => {
+    return (
+        <div>ProfilePicture</div>
+    )
+}
+
+export default ProfilePicture
