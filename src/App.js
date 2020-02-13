0b9b526f55e9e8d6dfad73ba3968adffc5139f34
yboodhan <yboodhan@gmail.com>
2020-02-13 13:49:21 -0800

    Fixed errors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom'
 import './App.css';
 
 import Content from './Components/Content'
-import Profile from './Components/Profile';
 import NavBottom from './Components/NavBottom';
 
 function App() {
