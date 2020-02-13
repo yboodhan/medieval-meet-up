9fca3b157eac60ece8e17fe2842b9682ccec1e5a
yboodhan <yboodhan@gmail.com>
2020-02-13 11:39:40 -0800

    Added outline of profile component

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Col, Row } from 'reactstrap';
+
+const Profile = props => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Profile
