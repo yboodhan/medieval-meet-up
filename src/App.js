c2a23e000684f6aff9980a2bbaf9e138e1fff98c
yboodhan <yboodhan@gmail.com>
2020-02-13 13:22:35 -0800

    Added buttons to nav bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import Profile from './Components/Profile';
+import NavBottom from './Components/NavBottom';
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <div className="nav-bar">
+        <NavBottom />
+      </div>
     </div>
   );
 }
--- src/Components/NavBottom.jsx
+++ src/Components/NavBottom.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Container, Col, Row } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faCog, faHeart, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const NavBottom = props => {
     return (
-        <div>NavBar</div>
+        <Container>
+                <Row>
+                    <Col>
+                        <div className="nav-button-bar">
+                            <div className="nav-button">
+                                <FontAwesomeIcon size="3x" icon={faHome} />
+                            </div>
+                            <div className="nav-button">
+                                <FontAwesomeIcon size="3x" icon={faCog} />
+                            </div>
+                            <div className="nav-button">
+                                <FontAwesomeIcon size="3x" icon={faHeart} />
+                            </div>
+                            <div className="nav-button">
+                                <FontAwesomeIcon size="3x" icon={faEnvelope} />
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+        </Container>
     )
 }
 
