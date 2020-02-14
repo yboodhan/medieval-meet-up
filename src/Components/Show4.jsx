6cd183843b9d1f295cdca51b2a30265a7ee10e3a
yboodhan <yboodhan@gmail.com>
2020-02-14 14:30:00 -0800

    DONE

--- src/Components/Show4.jsx
+++ src/Components/Show4.jsx
@@ -4,8 +4,8 @@ import MatchProfile from './MatchProfile';
 let user = {
     id: 4,
     name: 'Big Bad Wolf',
-    interest: 'Thighs, legs, arms, any meat really',
-    about: 'Like hairy guys? Looking for a Big Bad boy to huff and puff and blow your mind? Not all the guys are wolves, but this one is.',
+    interest: 'Thighs, legs, arms, any meat',
+    about: 'Like hairy guys? Looking for a Big Bad boy to huff and puff and blow your mind?',
     picture: ['https://i.pinimg.com/originals/1f/c0/58/1fc0584cfa9234edd54bce17f430c37b.jpg']
 }
 
