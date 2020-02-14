1179bf8badd2f123b3aa8916c84fd9065249c791
yboodhan <yboodhan@gmail.com>
2020-02-13 19:32:17 -0800

    Added all settings

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Form, Input, Label, FormGroup } from 'reactstrap';
+import { Container, Col, Row, Form, Input, Label, FormGroup } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faCog, faHeart, faComment } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,19 +7,124 @@ const Settings = props => {
     return (
         <Container className="settings-page">
             <h1>Filter Your Matches</h1>
-            <Form>
+            <div className="settings">
+                <Form>
+                    <FormGroup>
+                        <Label className="label">Location:</Label>
+                        <Input className="input" type="select" name="location" id="location">
+                            <option>Any</option>
+                            <option>Pirate Island</option>
+                            <option>Doloc</option>
+                            <option>Sherwood Forest</option>
+                            <option>Merlin's Island</option>
+                            <option>Neverland</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label className="label">Within:</Label>
+                        <Input className="input" type="number" name="distance" id="distance">
+                        </Input> miles
+                    </FormGroup>
+                    <FormGroup>
+                        <Label className="label">Age:</Label>
+                        <Input className="input" type="select" name="age" id="age">
+                            <option>Any</option>
+                            <option>18-25</option>
+                            <option>26-35</option>
+                            <option>36-45</option>
+                            <option>46-55</option>
+                            <option>56+</option>
+                        </Input>
+                    </FormGroup>
+                    <br/>
+                    <FormGroup check>
+                        <Label className="label">Preferences:</Label>
+                    </FormGroup>
+                </Form>
+            </div>
+
+            <div className="preferences">
                 <FormGroup>
-                    <Label className="label">Location:</Label>
-                    <Input className="input" type="select" name="location" id="location">
-                        <option>Any</option>
-                        <option>Pirate Island</option>
-                        <option>Doloc</option>
-                        <option>Sherwood Forest</option>
-                        <option>Merlin's Island</option>
-                        <option>Neverland</option>
-                    </Input>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Humans 🤷🏽‍♀️
+                    </Label>
                 </FormGroup>
-            </Form>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Ogres 🌳
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Donkeys 🎶
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Cats 🐈
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Dwarves 📏
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Fairies 🧚🏻‍♂️
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Eggs 🥚
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Cookies 🍪
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Woods 🌲
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Pigs 🐖
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Wolves 🐺
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Mice 🐭
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Cyclops 👁
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Gnomes 🌈
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label check>
+                        <Input className="input" type="checkbox"/> Any 💚
+                    </Label>
+                </FormGroup>
+            </div>
+            <a href="/">
+                <button className="button-style">
+                    Update
+                </button>
+            </a>
         </Container>
     )
 }
